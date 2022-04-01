579c948ab44d58cec7adc198c99a51e4e9cb1d11
victor_titov <victor8titov@yandex.ru>
2022-04-01 17:36:28 +0300

    init

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+
+  extends: [
+    'standard',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+
+  plugins: [
+    '@typescript-eslint',
+    'react-hooks'
+  ],
+
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/prop-types': 'off',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error']
+  },
+  settings: {
+    react: {
+      pragma: 'React',
+      version: 'detect'
+    }
+  }
+}
